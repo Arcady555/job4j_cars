49dead11b86de0f881c70fd5acc4b1f3d57f9dc7
Arcady555 <parfenov7233@gmail.com>
2022-10-28 12:56:15 +0300

    HQL [#6874]-2

--- src/main/java/ru/job4j/model/repository/UserRepository.java
+++ src/main/java/ru/job4j/model/repository/UserRepository.java
@@ -1,13 +1,10 @@
 package ru.job4j.model.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 import ru.job4j.model.User;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -16,27 +13,7 @@ import java.util.Optional;
 public class UserRepository {
     private final SessionFactory sf;
 
-    /**
-     * Сохранить в базе.
-     * @param user пользователь.
-     * @return пользователь с id.
-     */
     public User create(User user) {
-      /**
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.createQuery(
-                            "INSERT INTO auto_user (login, password) VALUES  (:fLogin, :fPassword);")
-                    .setParameter("fLogin", user.getLogin())
-                    .setParameter("fPassword", user.getPassword())
-                    .executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-       */
         Session session = sf.openSession();
         session.beginTransaction();
         session.save(user);
@@ -45,10 +22,6 @@ public class UserRepository {
         return user;
     }
 
-    /**
-     * Обновить в базе пользователя.
-     * @param user пользователь.
-     */
     public void update(User user) {
         Session session = sf.openSession();
         try {
@@ -66,10 +39,6 @@ public class UserRepository {
         }
     }
 
-    /**
-     * Удалить пользователя по id.
-     * @param userId ID
-     */
     public void delete(int userId) {
         Session session = sf.openSession();
         try {
@@ -85,10 +54,6 @@ public class UserRepository {
         }
     }
 
-    /**
-     * Список пользователь отсортированных по id.
-     * @return список пользователей.
-     */
     public List<User> findAllOrderById() {
         Session session = sf.openSession();
         session.beginTransaction();
@@ -100,10 +65,6 @@ public class UserRepository {
         return result;
     }
 
-    /**
-     * Найти пользователя по ID
-     * @return пользователь.
-     */
     public Optional<User> findById(int userId) {
         Optional<User> result = Optional.empty();
         Session session = sf.openSession();
@@ -118,51 +79,24 @@ public class UserRepository {
         return result;
     }
 
-    /**
-     * Список пользователей по login LIKE %key%
-     * @param key key
-     * @return список пользователей.
-     */
     public List<User> findByLikeLogin(String key) {
-        List<User> result = new ArrayList<>();
         Session session = sf.openSession();
         session.beginTransaction();
-        List list = session.createQuery("from User").list();
-        List<User> userList = (List<User>) list;
-        for (User user : userList) {
-            if (user.getLogin().contains(key)) {
-                result.add(user);
-            }
-        }
+        List result = session.createQuery("from User as i where i.login like :fkey")
+                .setParameter("fkey", key)
+                .list();
         session.getTransaction().commit();
         session.close();
         return result;
     }
 
-    /**
-     * Найти пользователя по login.
-     * @param login login.
-     * @return Optional or user.
-     */
     public Optional<User> findByLogin(String login) {
-    /**
-        Optional<User> result = Optional.empty();
         Session session = sf.openSession();
         session.beginTransaction();
-        session.createQuery(
-                        "from User as i where i.login = :fLogin", User.class)
-                .setParameter("fLogin", login);
-        User user =session.get(User.class, id);
-        result = Optional.of(user);
+        Optional<User> result = session.createQuery("from User as i where i.login = :fLogin", User.class)
+                .setParameter("fLogin", login)
+                .uniqueResultOptional();
         session.getTransaction().commit();
-        session.close();
-        return result; */
-        Optional<User> result = Optional.empty();
-        Session session = sf.openSession();
-        Criteria userCriteria = session.createCriteria(User.class);
-        userCriteria.add(Restrictions.eq("login", login));
-        User user = (User) userCriteria.uniqueResult();
-        result = Optional.of(user);
         session.close();
         return result;
     }
